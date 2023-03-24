a85c3c54694f9ee6052f4d94d96a937b05bd16cc
Kosi Uhuegbu <kosiuhuegbu@gmail.com>
2023-03-24 11:03:40 +0000

    gitpod.yml update, requirement.txt update, app.py update, xray json

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -22,12 +22,23 @@ from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExport
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
+# xray
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+
+
 # Initialize tracing and an exporter that can send data to Honeycomb
 provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
+
+# xray
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+XRayMiddleware(app, xray_recorder)
+
 
 # Initialize automatic instrumentation with Flask
 # comment app=flask line because it exists already underneath
