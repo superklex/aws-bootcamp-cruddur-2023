68665256ee6063c898a63b94c332d6d4a3afcff8
Kosi Uhuegbu <kosiuhuegbu@gmail.com>
2023-03-31 18:11:21 +0000

    replacing cookies with real cognito auth

--- frontend-react-js/src/components/DesktopSidebar.js
+++ frontend-react-js/src/components/DesktopSidebar.js
@@ -5,43 +5,36 @@ import SuggestedUsersSection from '../components/SuggestedUsersSection'
 import JoinSection from '../components/JoinSection'
 
 export default function DesktopSidebar(props) {
-  const trendings = [
-    {"hashtag": "100DaysOfCloud", "count": 2053 },
-    {"hashtag": "CloudProject", "count": 8253 },
-    {"hashtag": "AWS", "count": 9053 },
-    {"hashtag": "FreeWillyReboot", "count": 7753 }
-  ]
+	const trendings = [
+		{ hashtag: '100DaysOfCloud', count: 2053 },
+		{ hashtag: 'CloudProject', count: 8253 },
+		{ hashtag: 'AWS', count: 9053 },
+		{ hashtag: 'FreeWillyReboot', count: 7753 },
+	];
 
-  const users = [
-    {"display_name": "Andrew Brown", "handle": "andrewbrown"}
-  ]
+	const users = [{ display_name: 'Andrew Brown', handle: 'andrewbrown' }];
 
-  let trending;
-  if (props.user) {
-    trending = <TrendingSection trendings={trendings} />
-  }
+	let trending;
+	let suggested;
+	let join;
+	if (props.user) {
+		trending = <TrendingSection trendings={trendings} />;
+		suggested = <SuggestedUsersSection users={users} />;
+	} else {
+		join = <JoinSection />;
+	}
 
-  let suggested;
-  if (props.user) {
-    suggested = <SuggestedUsersSection users={users} />
-  }
-  let join;
-  if (props.user) {
-  } else {
-    join = <JoinSection />
-  }
-
-  return (
-    <section>
-      <Search />
-      {trending}
-      {suggested}
-      {join}
-      <footer>
-        <a href="#">About</a>
-        <a href="#">Terms of Service</a>
-        <a href="#">Privacy Policy</a>
-      </footer>
-    </section>
-  );
+	return (
+		<section>
+			<Search />
+			{trending}
+			{suggested}
+			{join}
+			<footer>
+				<a href='#'>About</a>
+				<a href='#'>Terms of Service</a>
+				<a href='#'>Privacy Policy</a>
+			</footer>
+		</section>
+	);
 }
